2d8a15458834fa81a3cea6f959afcf7c049dab2e
samuel07119 <samuel.gavirial@uqvirtual.edu.co>
2025-08-27 07:51:21 -0500

    veterinaria

---
+++ veterinaria/src/main/java/org/example/Veterinario.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Veterinario {
+    private String nombre;
+    private String id;
+
+
+    @Override
+    public String toString() {
+        return "Veterinario{" +
+                "nombre='" + nombre + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    public Veterinario (String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+}
+
